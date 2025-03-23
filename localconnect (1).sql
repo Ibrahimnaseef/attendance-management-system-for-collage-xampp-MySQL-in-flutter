9fcf90cadf735bd5b9fa388f4dc93c0c244d6ad8
Ibrahimnaseef <ibrahimnaseefkj@gmail.com>
2025-03-23 18:09:05 +0530

    Add files via upload

--- localconnect (1).sql
+++ localconnect (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 09, 2025 at 11:00 AM
+-- Generation Time: Mar 21, 2025 at 07:24 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -40,6 +40,80 @@ CREATE TABLE `admin` (
 
 INSERT INTO `admin` (`id`, `user_id`, `name`, `email`) VALUES
 (1, 1, 'Admin1', 'admin@example.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `classes`
+--
+
+CREATE TABLE `classes` (
+  `id` int(11) NOT NULL,
+  `department_id` int(11) NOT NULL,
+  `semester` int(11) NOT NULL,
+  `class_name` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `classes`
+--
+
+INSERT INTO `classes` (`id`, `department_id`, `semester`, `class_name`, `created_at`) VALUES
+(1, 1, 1, 'Semester 1 - Department 1', '2025-03-12 18:54:12'),
+(2, 1, 2, 'Semester 2 - Department 1', '2025-03-12 18:54:12'),
+(3, 1, 3, 'Semester 3 - Department 1', '2025-03-12 18:54:12'),
+(4, 1, 4, 'Semester 4 - Department 1', '2025-03-12 18:54:12'),
+(5, 1, 5, 'Semester 5 - Department 1', '2025-03-12 18:54:12'),
+(6, 1, 6, 'Semester 6 - Department 1', '2025-03-12 18:54:12'),
+(7, 1, 7, 'Semester 7 - Department 1', '2025-03-12 18:54:12'),
+(8, 1, 8, 'Semester 8 - Department 1', '2025-03-12 18:54:12'),
+(9, 2, 1, 'Semester 1 - Department 2', '2025-03-12 18:54:12'),
+(10, 2, 2, 'Semester 2 - Department 2', '2025-03-12 18:54:12'),
+(11, 2, 3, 'Semester 3 - Department 2', '2025-03-12 18:54:12'),
+(12, 2, 4, 'Semester 4 - Department 2', '2025-03-12 18:54:12'),
+(13, 2, 5, 'Semester 5 - Department 2', '2025-03-12 18:54:12'),
+(14, 2, 6, 'Semester 6 - Department 2', '2025-03-12 18:54:12'),
+(15, 2, 7, 'Semester 7 - Department 2', '2025-03-12 18:54:12'),
+(16, 2, 8, 'Semester 8 - Department 2', '2025-03-12 18:54:12'),
+(17, 3, 1, 'Semester 1 - Department 3', '2025-03-12 18:54:12'),
+(18, 3, 2, 'Semester 2 - Department 3', '2025-03-12 18:54:12'),
+(19, 3, 3, 'Semester 3 - Department 3', '2025-03-12 18:54:12'),
+(20, 3, 4, 'Semester 4 - Department 3', '2025-03-12 18:54:12'),
+(21, 3, 5, 'Semester 5 - Department 3', '2025-03-12 18:54:12'),
+(22, 3, 6, 'Semester 6 - Department 3', '2025-03-12 18:54:12'),
+(23, 3, 7, 'Semester 7 - Department 3', '2025-03-12 18:54:12'),
+(24, 3, 8, 'Semester 8 - Department 3', '2025-03-12 18:54:12'),
+(25, 4, 1, 'Semester 1 - Department 4', '2025-03-12 18:54:12'),
+(26, 4, 2, 'Semester 2 - Department 4', '2025-03-12 18:54:12'),
+(27, 4, 3, 'Semester 3 - Department 4', '2025-03-12 18:54:12'),
+(28, 4, 4, 'Semester 4 - Department 4', '2025-03-12 18:54:12'),
+(29, 4, 5, 'Semester 5 - Department 4', '2025-03-12 18:54:12'),
+(30, 4, 6, 'Semester 6 - Department 4', '2025-03-12 18:54:12'),
+(31, 4, 7, 'Semester 7 - Department 4', '2025-03-12 18:54:12'),
+(32, 4, 8, 'Semester 8 - Department 4', '2025-03-12 18:54:12'),
+(33, 5, 1, 'Semester 1 - Department 5', '2025-03-12 18:54:12'),
+(34, 5, 2, 'Semester 2 - Department 5', '2025-03-12 18:54:12'),
+(35, 5, 3, 'Semester 3 - Department 5', '2025-03-12 18:54:12'),
+(36, 5, 4, 'Semester 4 - Department 5', '2025-03-12 18:54:12'),
+(37, 5, 5, 'Semester 5 - Department 5', '2025-03-12 18:54:12'),
+(38, 5, 6, 'Semester 6 - Department 5', '2025-03-12 18:54:12'),
+(39, 5, 7, 'Semester 7 - Department 5', '2025-03-12 18:54:12'),
+(40, 5, 8, 'Semester 8 - Department 5', '2025-03-12 18:54:12');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `class_subjects`
+--
+
+CREATE TABLE `class_subjects` (
+  `id` int(11) NOT NULL,
+  `class_id` int(11) DEFAULT NULL,
+  `subject_id` int(11) DEFAULT NULL,
+  `faculty_id` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -88,7 +162,11 @@ INSERT INTO `faculty` (`id`, `user_id`, `name`, `department_id`, `email`) VALUES
 (23, 28, 'faculty2', 'CSE', 'faculty2@gmail.com'),
 (25, 31, 'faculty3', 'ECE', 'faculty3@gmail.com'),
 (26, 32, 'faculty4', 'ECE', 'faculty4@gmail.com'),
-(27, 33, 'faculty5', 'CYS', 'faculty5@gmail.com');
+(27, 33, 'faculty5', 'CYS', 'faculty5@gmail.com'),
+(28, 36, 'faculty6', 'PO', 'faculty6@gmail.com'),
+(29, 37, 'faculty7', 'PO', 'faculty7@gmail.com'),
+(34, 120, 'teacher3', 'ECE', 'teacher3@gmail.com'),
+(35, 121, 'teacher2', 'CSE', 'teacher2@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -98,15 +176,23 @@ INSERT INTO `faculty` (`id`, `user_id`, `name`, `department_id`, `email`) VALUES
 
 CREATE TABLE `semesters` (
   `id` int(11) NOT NULL,
-  `current_semester` varchar(11) NOT NULL
+  `semester` enum('S1','S2','S3','S4','S5','S6','S7','S8') NOT NULL,
+  `semester_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `semesters`
 --
 
-INSERT INTO `semesters` (`id`, `current_semester`) VALUES
-(1, 'S1');
+INSERT INTO `semesters` (`id`, `semester`, `semester_id`) VALUES
+(1, 'S1', 1),
+(2, 'S2', 2),
+(3, 'S3', 3),
+(4, 'S4', 4),
+(5, 'S5', 5),
+(6, 'S6', 6),
+(7, 'S7', 7),
+(8, 'S8', 8);
 
 -- --------------------------------------------------------
 
@@ -120,15 +206,85 @@ CREATE TABLE `students` (
   `name` varchar(255) NOT NULL,
   `department_id` varchar(255) DEFAULT NULL,
   `email` varchar(255) NOT NULL,
-  `semester` varchar(11) DEFAULT '1'
+  `semester` enum('S1','S2','S3','S4','S5','S6','S7','S8') NOT NULL,
+  `semester_st` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `students`
 --
 
-INSERT INTO `students` (`id`, `user_id`, `name`, `department_id`, `email`, `semester`) VALUES
-(1, 2, 'student1', 'CSE', 'student@example.com', 'S1');
+INSERT INTO `students` (`id`, `user_id`, `name`, `department_id`, `email`, `semester`, `semester_st`) VALUES
+(3, 35, 'student', 'CSE', 'student@gmail.com', 'S6', 6),
+(4, 38, 'student2', 'ECE', 'student2@gmail.com', 'S3', 3),
+(7, 41, 'student3', 'ECE', 'student3@gmail.com', 'S3', 3),
+(14, 128, 'adhil', 'CSE', 'adhilkhakkim@gmail.com', 'S6', 6),
+(15, 130, 'lathee', 'CSE', 'lathee@gmail.com', 'S4', 4),
+(16, 132, 'student8', 'ECE', 'student8@gmail.com', 'S3', 3),
+(17, 133, 'student9', 'CSE', 'student9@gmail.com', 'S5', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students_backup`
+--
+
+CREATE TABLE `students_backup` (
+  `id` int(11) NOT NULL DEFAULT 0,
+  `user_id` int(11) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `department_id` varchar(255) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `semester` enum('S1','S2','S3','S4','S5','S6','S7','S8') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subjects`
+--
+
+CREATE TABLE `subjects` (
+  `id` int(11) NOT NULL,
+  `department_id` int(11) DEFAULT NULL,
+  `semester` int(11) NOT NULL,
+  `subject_name` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `start_date` date DEFAULT NULL,
+  `end_date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `subjects`
+--
+
+INSERT INTO `subjects` (`id`, `department_id`, `semester`, `subject_name`, `created_at`, `start_date`, `end_date`) VALUES
+(1, 1, 2, 'data structure', '2025-03-12 19:42:19', NULL, NULL),
+(2, 2, 2, 'CYBER', '2025-03-13 08:18:43', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subjects_faculty`
+--
+
+CREATE TABLE `subjects_faculty` (
+  `id` int(11) NOT NULL,
+  `department_id` int(11) NOT NULL,
+  `semester` int(10) NOT NULL,
+  `subject_id` int(11) NOT NULL,
+  `faculty_id` int(10) NOT NULL,
+  `start_date` date DEFAULT NULL,
+  `end_date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `subjects_faculty`
+--
+
+INSERT INTO `subjects_faculty` (`id`, `department_id`, `semester`, `subject_id`, `faculty_id`, `start_date`, `end_date`) VALUES
+(8, 1, 2, 1, 28, NULL, NULL),
+(9, 2, 2, 2, 2, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -149,13 +305,23 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `username`, `password`, `role`) VALUES
 (1, 'PUCE22', '123456', 'admin'),
-(2, 'UCE22', '12345', 'student'),
 (3, 'FUCE22', '123456', 'faculty'),
 (4, 'FUCE221', '123456', 'faculty'),
-(28, 'FUCE23', '$2y$10$8F1A1qKg4DbZRbKS0tKjy.UK4trP2gkb4BuIMjcrcS1nXTt.yakkC', 'faculty'),
-(31, 'FUCE24', '$2y$10$TvdxfNnXJCr.ht1mDAGb0OFeL1DhzBcNRt3n5umhFxTDrKDWCYVFy', 'faculty'),
-(32, 'FUCE25', '$2y$10$T.jsr5cr.X4c56YhyklQTObwHkCwqjQ0LePUozBWa9FxNHkdddaH2', 'faculty'),
-(33, 'FUCE26', '$2y$10$tN2hNwDxLYjMIJjhB5SCyeGZAQHBBbv4ABUH./wl0oAToSFB9GjDy', 'faculty');
+(28, 'FUCE23', '123456', 'faculty'),
+(31, 'FUCE24', '123456', 'faculty'),
+(32, 'FUCE25', '123456', 'faculty'),
+(33, 'FUCE26', '123456', 'faculty'),
+(35, 'UCE22', '123456', 'student'),
+(36, 'FUCE27', '123456', 'faculty'),
+(37, 'FUCE28', '123456', 'faculty'),
+(38, 'UCE24', '123456', 'student'),
+(41, 'UCE25', '123456', 'student'),
+(120, 'FUCE29', '123456', 'faculty'),
+(121, 'FUCE30', '123456', 'faculty'),
+(128, 'UCE22CS048', 'adhil123', 'student'),
+(130, 'UCE22CS3', '123456', 'student'),
+(132, 'UCE29', '123456', 'student'),
+(133, 'UCE30', '123456', 'student');
 
 --
 -- Indexes for dumped tables
@@ -168,6 +334,23 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`),
   ADD UNIQUE KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `classes`
+--
+ALTER TABLE `classes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `department_id` (`department_id`),
+  ADD KEY `semester` (`semester`);
+
+--
+-- Indexes for table `class_subjects`
+--
+ALTER TABLE `class_subjects`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `class_id` (`class_id`),
+  ADD KEY `subject_id` (`subject_id`),
+  ADD KEY `faculty_id` (`faculty_id`);
 
 --
 -- Indexes for table `departments`
@@ -189,7 +372,8 @@ ALTER TABLE `faculty`
 -- Indexes for table `semesters`
 --
 ALTER TABLE `semesters`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `semester_id` (`semester_id`);
 
 --
 -- Indexes for table `students`
@@ -198,7 +382,25 @@ ALTER TABLE `students`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`),
   ADD UNIQUE KEY `user_id` (`user_id`),
-  ADD KEY `department_id` (`department_id`);
+  ADD KEY `department_id` (`department_id`),
+  ADD KEY `semester_st` (`semester_st`) USING BTREE;
+
+--
+-- Indexes for table `subjects`
+--
+ALTER TABLE `subjects`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `department_id` (`department_id`),
+  ADD KEY `subjects_ibfk_1` (`semester`);
+
+--
+-- Indexes for table `subjects_faculty`
+--
+ALTER TABLE `subjects_faculty`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `subject_id` (`subject_id`),
+  ADD KEY `dept` (`department_id`),
+  ADD KEY `faculty` (`faculty_id`);
 
 --
 -- Indexes for table `users`
@@ -218,6 +420,18 @@ ALTER TABLE `admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `classes`
+--
+ALTER TABLE `classes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+
+--
+-- AUTO_INCREMENT for table `class_subjects`
+--
+ALTER TABLE `class_subjects`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `departments`
 --
 ALTER TABLE `departments`
@@ -227,25 +441,37 @@ ALTER TABLE `departments`
 -- AUTO_INCREMENT for table `faculty`
 --
 ALTER TABLE `faculty`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT for table `semesters`
 --
 ALTER TABLE `semesters`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `subjects`
+--
+ALTER TABLE `subjects`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `subjects_faculty`
+--
+ALTER TABLE `subjects_faculty`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=134;
 
 --
 -- Constraints for dumped tables
@@ -256,6 +482,21 @@ ALTER TABLE `users`
 --
 ALTER TABLE `admin`
   ADD CONSTRAINT `admin_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `classes`
+--
+ALTER TABLE `classes`
+  ADD CONSTRAINT `classes_ibfk_1` FOREIGN KEY (`department_id`) REFERENCES `departments` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `classes_ibfk_2` FOREIGN KEY (`semester`) REFERENCES `semesters` (`id`);
+
+--
+-- Constraints for table `class_subjects`
+--
+ALTER TABLE `class_subjects`
+  ADD CONSTRAINT `class_subjects_ibfk_1` FOREIGN KEY (`class_id`) REFERENCES `classes` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `class_subjects_ibfk_2` FOREIGN KEY (`subject_id`) REFERENCES `subjects` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `class_subjects_ibfk_3` FOREIGN KEY (`faculty_id`) REFERENCES `faculty` (`id`) ON DELETE SET NULL;
 
 --
 -- Constraints for table `faculty`
@@ -269,7 +510,22 @@ ALTER TABLE `faculty`
 --
 ALTER TABLE `students`
   ADD CONSTRAINT `students_ibfk_1` FOREIGN KEY (`department_id`) REFERENCES `departments` (`depid`) ON DELETE CASCADE,
-  ADD CONSTRAINT `students_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `students_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `students_ibfk_3` FOREIGN KEY (`semester_st`) REFERENCES `semesters` (`semester_id`);
+
+--
+-- Constraints for table `subjects`
+--
+ALTER TABLE `subjects`
+  ADD CONSTRAINT `subjects_ibfk_1` FOREIGN KEY (`semester`) REFERENCES `semesters` (`semester_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `subjects_faculty`
+--
+ALTER TABLE `subjects_faculty`
+  ADD CONSTRAINT `dept` FOREIGN KEY (`department_id`) REFERENCES `departments` (`id`),
+  ADD CONSTRAINT `faculty` FOREIGN KEY (`faculty_id`) REFERENCES `faculty` (`id`),
+  ADD CONSTRAINT `subject_id` FOREIGN KEY (`subject_id`) REFERENCES `subjects` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
